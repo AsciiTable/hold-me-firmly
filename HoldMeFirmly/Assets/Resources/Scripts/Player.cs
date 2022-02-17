189ec1a472ffa8d0898c73234d3c1b5b7e374111
Jessica Wei <jessicawei9@gmail.com>
2022-02-17 01:30:32 -0800

    + Shapes that roll off the plate + oh yea, tilt works now

--- HoldMeFirmly/Assets/Resources/Scripts/Player.cs
+++ HoldMeFirmly/Assets/Resources/Scripts/Player.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private const float CIRCLE_DEGREES = 360.0f;
     [SerializeField] private float movementSpeed = 10.0f;
     [SerializeField] private float tiltSpeed = 5.0f;
+    [SerializeField] private float maxTilt = 30.0f;
     [SerializeField] private float energyExpenditureSpeed = 2.0f;
     [SerializeField] private float totalEnergy = 100.0f;
     private float currentEnergy = 0.0f;
@@ -21,6 +23,8 @@ public class Player : MonoBehaviour
     void Update(){
         if(Input.GetButton("Horizontal"))
             MovePlayer();
+        if (Input.GetButton("Tilt"))
+            TiltPlayer();
     }
 
     private void MovePlayer() {
@@ -28,5 +32,26 @@ public class Player : MonoBehaviour
             gameObject.transform.position += new Vector3(movementSpeed * Time.deltaTime, 0.0f, 0.0f);
         else
             gameObject.transform.position += new Vector3(-1.0f * movementSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    // would want strap underneath plate to prevent it from falling off from hand
+    private void TiltPlayer() {
+        float eulerZ = gameObject.transform.localEulerAngles.z;
+        if (Input.GetAxis("Tilt") > 0) { // Tilt to the left
+            if (eulerZ < maxTilt || (eulerZ > maxTilt && eulerZ >= CIRCLE_DEGREES - maxTilt - 0.01f)) {
+                gameObject.transform.localEulerAngles += new Vector3(0.0f, 0.0f, tiltSpeed * Time.deltaTime);
+                if (gameObject.transform.localEulerAngles.z > maxTilt && gameObject.transform.localEulerAngles.z < CIRCLE_DEGREES - maxTilt) {
+                    gameObject.transform.localEulerAngles = new Vector3(0.0f, 0.0f, maxTilt);
+                }
+            }
+        }
+        else if (Input.GetAxis("Tilt") < 0) { // Tilt to the right
+            if ((eulerZ > CIRCLE_DEGREES - maxTilt) || (eulerZ <= maxTilt + 0.01f)) {
+                gameObject.transform.localEulerAngles += new Vector3(0.0f, 0.0f, -1.0f * tiltSpeed * Time.deltaTime);
+                if (gameObject.transform.localEulerAngles.z < CIRCLE_DEGREES - maxTilt && gameObject.transform.localEulerAngles.z > maxTilt){
+                    gameObject.transform.localEulerAngles = new Vector3(0.0f, 0.0f, CIRCLE_DEGREES - maxTilt);
+                }
+            }
+        }
     }
 }
